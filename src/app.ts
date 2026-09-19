92a7513b19b7800f77effa9d70c6a44788c09038
DefNotArham <arhamkabir231@gmail.com>
2026-09-19 10:52:00 -0400

    initialize express api

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  console.log("TEST ROUTE HIT");
+
+  res.json({
+    status: "ok",
+  });
+});
+
+export default app;
