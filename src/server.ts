b3ef5ed41ab9fd551f86faebae7df6bd8f2348af
DefNotArham <arhamkabir231@gmail.com>
2026-09-23 21:46:44 -0400

    connect to db

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app.js";
+import connectDb from "./config/database.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
